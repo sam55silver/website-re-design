f9f1e84a853e0a9a5fdab90f5361657c4df53bf7
Sam Silver <sam55silver@gmail.com>
2022-11-21 20:24:41 -0400

    final changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,13 @@ module.exports = {
   content: ['./*.{html,js}'],
   theme: {
     fontFamily: {
-      'serif': ['PT Serif Caption'],
+      'serif': ['PT Serif Caption', 'Playfair Display'],
+      'title': ['Playfair Display'],
     },
     extend: {
       colors: {
-        'foreground': '#749f82',
+        'foreground': '#6a9478',
+        'foreground-darker': '#4c6b56',
         'focus': '#d9bd8f',
       },
     },
